9e0b55cb315f6292ee753e249586afd5655be697
HiiroAyane <psyr2y@outlook.com>
2022-03-16 22:10:27 +0800

    custom ethpawd

--- main.cpp
+++ main.cpp
@@ -9,9 +9,9 @@ int main(int argc, char* argv[])
         return -1;
     }
     if (argc == 1) {
-        printf("-d:deploy cess by params.txt\nparams.txt格式:\n矿工号\n第二账户私钥\n本机公网IP\n挖矿硬盘挂载路径\n挖矿硬盘容量（TB）\n");
+        printf("-d:deploy cess by params.txt\nparams.txt格式:\n矿工号\n第二账户私钥\n第二账户Eth密码（自定义）\n本机公网IP\n挖矿硬盘挂载路径\n挖矿硬盘容量（TB）\n");
         printf("如：\n");
-        printf("C419\n0000000000000000000000000000000000000000000000000000000000000000\n113.207.100.100\n/lotus_cache\n10\n");
+        printf("C419\n0000000000000000000000000000000000000000000000000000000000000000\nethPasswd\n113.207.100.100\n/lotus_cache\n10\n");
     }
     else if (argc == 2) {
         string strArgs = argv[1];
